05ffbacc42ccbbd4a6bd44b3f421808c833b3662
stephen <lee.stephen@courrier.uqam.ca>
2024-06-12 16:10:37 -0400

    init backend

---
+++ demo/src/main/java/com/JobReferral/demo/JobReferralApplication.java
@@ -0,0 +1,13 @@
+package com.JobReferral.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobReferralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobReferralApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/JobReferral/demo/JobReferralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JobReferral.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobReferralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
